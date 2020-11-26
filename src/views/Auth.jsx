84700d7aebe50317ffdd83f61adccad1ae9232aa
Alisson <thelinuxlich@gmail.com>
2020-11-26 20:05:40 -0300

    changed some HTML attributes to be React-compatible

--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const Auth = () => {
   return (
-    <div class='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
-      <div class='max-w-md w-full space-y-8'>
+    <div className='min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-md w-full space-y-8'>
         <div>
           <img
-            class='mx-auto h-12 w-auto'
+            className='mx-auto h-12 w-auto'
             src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
             alt='Workflow'
           />
-          <h2 class='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
             Sign in to your account
           </h2>
         </div>
-        <form class='mt-8 space-y-6' action='#' method='POST'>
+        <form className='mt-8 space-y-6' action='#' method='POST'>
           <input type='hidden' name='remember' value='true' />
-          <div class='rounded-md shadow-sm -space-y-px'>
+          <div className='rounded-md shadow-sm -space-y-px'>
             <div>
-              <label for='email-address' class='sr-only'>
+              <label htmlFor='email-address' className='sr-only'>
                 Email address
               </label>
               <input
@@ -26,12 +26,12 @@ const Auth = () => {
                 name='email'
                 type='email'
                 required
-                class='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                 placeholder='Email address'
               />
             </div>
             <div>
-              <label for='password' class='sr-only'>
+              <label htmlFor='password' className='sr-only'>
                 Password
               </label>
               <input
@@ -39,28 +39,31 @@ const Auth = () => {
                 name='password'
                 type='password'
                 required
-                class='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
+                className='appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm'
                 placeholder='Password'
               />
             </div>
           </div>
 
-          <div class='flex items-center justify-between'>
-            <div class='flex items-center'>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center'>
               <input
                 id='remember_me'
                 type='checkbox'
-                class='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+                className='h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
               />
-              <label for='remember_me' class='ml-2 block text-sm text-gray-900'>
+              <label
+                htmlFor='remember_me'
+                className='ml-2 block text-sm text-gray-900'
+              >
                 Remember me
               </label>
             </div>
 
-            <div class='text-sm'>
+            <div className='text-sm'>
               <a
                 href='#'
-                class='font-medium text-indigo-600 hover:text-indigo-500'
+                className='font-medium text-indigo-600 hover:text-indigo-500'
               >
                 Forgot your password?
               </a>
@@ -70,20 +73,20 @@ const Auth = () => {
           <div>
             <button
               type='submit'
-              class='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+              className='group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
             >
-              <span class='absolute left-0 inset-y-0 flex items-center pl-3'>
+              <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                 <svg
-                  class='h-5 w-5 text-indigo-500 group-hover:text-indigo-400'
+                  className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400'
                   xmlns='http://www.w3.org/2000/svg'
                   viewBox='0 0 20 20'
                   fill='currentColor'
                   aria-hidden='true'
                 >
                   <path
-                    fill-rule='evenodd'
+                    fillRule='evenodd'
                     d='M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z'
-                    clip-rule='evenodd'
+                    clipRule='evenodd'
                   />
                 </svg>
               </span>
